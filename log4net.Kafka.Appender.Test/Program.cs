cb959befd703efd766e1657d78c72e96c864bb4e
hayrullahcansu <hayrullah.cansu@gmail.com>
2018-05-02 17:54:59 +0300

    config and test case changed

--- log4net.Kafka.Appender.Test/Program.cs
+++ log4net.Kafka.Appender.Test/Program.cs
@@ -15,22 +15,15 @@ namespace log4net.Kafka.Appender.Test
         {
             ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
             GlobalContext.Properties["component"] = "send";
-            while (true)
-            {
 
-                logger.Debug("this Debug msg " + DateTime.Now.ToShortTimeString());
-                Thread.Sleep(1000);
-                logger.Warn("this Warn msg" + DateTime.Now.ToShortTimeString());
-                Thread.Sleep(1000);
-                logger.Info("this Info msg" + DateTime.Now.ToShortTimeString());
-                Thread.Sleep(1000);
-                logger.Error("this Error msg" + DateTime.Now.ToShortTimeString());
-                Thread.Sleep(1000);
-                logger.Fatal("this Fatal msg" + DateTime.Now.ToShortTimeString());
-                Thread.Sleep(7000);
-            }
-            Console.WriteLine("OK");
-            System.Console.ReadKey();
+
+            logger.Debug("this Debug msg " + DateTime.Now.ToShortTimeString());
+            //topic pattern  :   testapp-%property{component}-%class-%method-%level
+            //the log's topic:   testapp-send-log4net.Kafka.Appender.Test.Program-Main-DEBUG
+
+            //message pattern:   %d [%t] %-5p %c %m%n 
+            //message        :   2018-05-02 17:49:05,033 [1] DEBUG log4net.Kafka.Appender.Test.Program this Debug msg 5:49 PM
+
         }
     }
 }
